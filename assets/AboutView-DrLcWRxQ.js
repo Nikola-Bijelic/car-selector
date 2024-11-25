325a72df86552d16cf87506235db2d8ce8b44668
Nikola Bijelic <nikolabijelic14@gmail.com>
2024-11-25 16:33:26 +0100

    Updates

---
+++ assets/AboutView-DrLcWRxQ.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,a as e,o as s}from"./index-Ba9TeFhp.js";const n={},r={class:"about-view"};function c(u,o){return s(),a("div",r,o[0]||(o[0]=[e("h1",null,"About Us",-1),e("p",null,"This is the About page where you can add information about your app or company.",-1)]))}const l=t(n,[["render",c]]);export{l as default};
